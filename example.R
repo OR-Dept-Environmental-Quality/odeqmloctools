9b5d5694f63c55004e8d2be3551218e07fcba3a7
DEQrmichie <65993167+DEQrmichie@users.noreply.github.com>
2021-11-30 08:48:14 -0800

    Update example.R

--- example.R
+++ example.R
@@ -8,19 +8,15 @@ df.mloc2 <- df.mloc[complete.cases(df.mloc[,c("Latitude", "Longitude")]),]
 
 odeqmloctools::launch_map(mloc = df.mloc2)
 
-df2 <- df.mloc2 %>%
-  select(Longitude,Latitude, County.Name, State.Code) %>%
-  rowwise() %>%
-  mutate(#county = get_county(x=Longitude, y=Latitude),
-         #state = get_state(x=Longitude, y=Latitude),
-         get_huc8code(x=Longitude, y=Latitude)),
-         get_huc10code(x=Longitude, y=Latitude),
-         get_huc12code(x=Longitude, y=Latitude),
-         get_huc8name(x=Longitude, y=Latitude),
-         get_huc10name(x=Longitude, y=Latitude),
-         get_huc12name(x=Longitude, y=Latitude))
-
-
-
-get_huc8code(x=df.mloc2$Longitude[18], y=df.mloc2$Latitude[18])
+df.mloc3 <- df.mloc2 %>%
+  select(Longitude, Latitude, County.Name, State.Code) %>%
+  mutate(County.Name2 = get_county(x = Longitude, y = Latitude),
+         State.Code2 = get_state(x = Longitude, y = Latitude),
+         HUC8 = get_huc8code(x = Longitude, y = Latitude),
+         HUC10 = get_huc10code(x = Longitude, y = Latitude),
+         HUC12 = get_huc12code(x = Longitude, y = Latitude),
+         HUC8_Name = get_huc8name(x = Longitude, y = Latitude),
+         HUC10_Name = get_huc10name(x = Longitude, y = Latitude),
+         HUC12_Name = get_huc12name(x = Longitude, y = Latitude)
+)
 
