914b169708af78760e8ba2d1a140b69e90672ac0
DEQrmichie <65993167+DEQrmichie@users.noreply.github.com>
2021-12-07 08:31:09 -0800

    Update example.R

--- example.R
+++ example.R
@@ -1,22 +1,37 @@
 library(dplyr)
 library(odeqmloctools)
 
-df.mloc <- odeqcdr::contin_import(file = "/Users/rmichie/GitHub/odeqmloctools/test/mloc_example.xlsx",
-                                  sheets = c("Monitoring_Locations"))[["Monitoring_Locations"]]
+df.import <- odeqcdr::contin_import(file = "/Users/rmichie/GitHub/odeqmloctools/test/mloc_example.xlsx",
+                                    sheets = c("Monitoring_Locations"))[["Monitoring_Locations"]]
 
-df.mloc2 <- df.mloc[complete.cases(df.mloc[,c("Latitude", "Longitude")]),]
+# For this example pretend to only start with certain columns. Add CRS column.
+df.mloc <- df.import[complete.cases(df.import[,c("Latitude", "Longitude")]),]  %>%
+  select(Monitoring.Location.ID, Monitoring.Location.Name, Longitude, Latitude, Horizontal.Datum) %>%
+  dplyr::mutate(CRS = dplyr::case_when(Horizontal.Datum == "WGS84" ~ 4326,
+                                       Horizontal.Datum == "NAD83" ~ 4269,
+                                       TRUE ~ 4269))
 
-odeqmloctools::launch_map(mloc = df.mloc2)
+# Review and edit the information in the map
+df.mloc2 <- odeqmloctools::launch_map(mloc = df.mloc)
 
+# Add other data once the lat/long has been reviewed in launch map()
 df.mloc3 <- df.mloc2 %>%
-  select(Longitude, Latitude, County.Name, State.Code) %>%
-  mutate(County.Name2 = get_county(x = Longitude, y = Latitude),
-         State.Code2 = get_state(x = Longitude, y = Latitude),
-         HUC8 = get_huc8code(x = Longitude, y = Latitude),
-         HUC10 = get_huc10code(x = Longitude, y = Latitude),
-         HUC12 = get_huc12code(x = Longitude, y = Latitude),
-         HUC8_Name = get_huc8name(x = Longitude, y = Latitude),
-         HUC10_Name = get_huc10name(x = Longitude, y = Latitude),
-         HUC12_Name = get_huc12name(x = Longitude, y = Latitude)
-)
+  mutate(County.Name = get_county(x = Longitude, y = Latitude, crs = CRS),
+         State.Code = get_state(x = Longitude, y = Latitude, crs = CRS),
+         HUC8 = get_huc8code(x = Longitude, y = Latitude, crs = CRS),
+         HUC10 = get_huc10code(x = Longitude, y = Latitude, crs = CRS),
+         HUC12 = get_huc12code(x = Longitude, y = Latitude, crs = CRS),
+         HUC8_Name = get_huc8name(x = Longitude, y = Latitude, crs = CRS),
+         HUC10_Name = get_huc10name(x = Longitude, y = Latitude, crs = CRS),
+         HUC12_Name = get_huc12name(x = Longitude, y = Latitude, crs = CRS)
+  )
 
+#-- More examples ---
+
+# Get a data frame of most current NHD High from USGS. Includes measure value based on snapping x/y.
+df.nhd_usgs <- df.mloc3 %>%
+  get_nhd_df(x = Longitude, y = Latitude, crs = CRS, search_dist = 100, service = "USGS")
+
+# Get a data frame of NHD plus HR info
+df.nhdplus <- df.mloc3 %>%
+  get_nhdplus_df(x = Longitude, y = Latitude, crs = CRS, search_dist = 100)
