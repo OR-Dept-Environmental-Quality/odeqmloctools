caeed878667fd247a40b8550aafebf843181f6e4
DEQrmichie <michie.ryan@deq.state.or.us>
2022-04-08 10:32:15 -0700

    Create make_ornhd.R

---
+++ data_raw/make_ornhd.R
@@ -0,0 +1,20 @@
+library(readr)
+library(dplyr)
+
+# read gis txt export
+ornhd.raw <- readr::read_csv("data_raw/ornhd_2022_03.txt")
+
+# get the Strahler stream order from the older data frame
+# Once DEQ migrates to a new NHD version the StreamOrder field should be included
+# in the GIS export
+ornhd0 <- odeqmloctools::ornhd[, c("Permanent_Identifier", "StreamOrder")]
+
+ornhd <- ornhd.raw %>%
+  dplyr::left_join(ornhd0) %>%
+  dplyr::select(Permanent_Identifier, FDate, Resolution, GNIS_ID, GNIS_Name,
+                LengthKM, ReachCode, FlowDir, WBArea_Permanent_Identifier,
+                FType, FCode, MainPath, InNetwork, AU_ID, AU_Name, AU_Description,
+                AU_WBType, AU_UseCode, AU_GNIS_Name, AU_GNIS,
+                AU_LenMile, AU_AreaAcr, StreamOrder)
+
+save(ornhd, file="data/ornhd.RData")
