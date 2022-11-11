9af46936554b737c66925b3372801fc131b35051
DEQrmichie <ryan.michie@deq.oregon.gov>
2022-11-10 20:17:42 -0800

    update doc

--- R/mloc_example.R
+++ R/mloc_example.R
@@ -1,12 +1,13 @@
 #' Example Monitoring Location data
 #'
-#'Data frame of Oregon DEQ ambient network monitoring locations
+#' Data frame of Oregon DEQ ambient network monitoring locations
 #'
 #' @docType data
 #' @usage data(mloc_example)
 #' @keywords datasets
+#' @md
 #' @examples
-#' mlocs <- odeqmloctools::mloc_example()
+#' mlocs <- odeqmloctools::mloc_example
 #'
 
 "mloc_example"
