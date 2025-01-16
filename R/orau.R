c72f7dba21d7e70896fa483d1a1ffe3baa7e3f6d
DEQrmichie <65993167+DEQrmichie@users.noreply.github.com>
2025-01-16 09:26:00 -0800

    Update orau.R

--- R/orau.R
+++ R/orau.R
@@ -1,19 +1,18 @@
 #' Oregon DEQ Assessment Units
 #'
 #'Table of Oregon DEQ Assessment Units. The table corresponds
-#'to the final assessment units published with the 2022 Integrated Report.
-#'Version 03-30-2022.
+#'to the final assessment units published with the 2024 Integrated Report.
+#'Version 09-12-2024.
 #'
 #' \itemize{
 #'   \item AU_ID: Oregon assessment unit ID.
 #'   \item AU_Name: Name of the assessment unit.
-#'   \item AU_WBType: Assessment unit waterbody type code.
-#'   \item AU_UseCode: Assessment unit use code.
-#'   \item AQWMS_NUM: Unique ID formatted as numeric.
-#'   \item AQWMS_TXT: Unique ID formatted as text.
 #'   \item AU_Description: Assessment unit descriptions.
-#'   \item AU_LenMile: Assessment unit length in miles.
+#'   \item AU_Type: Assessment unit type.
+#'   \item AU_UseCode: Assessment unit use code.
+#'   \item AU_LenMiles: Assessment unit length in miles.
 #'   \item AU_AreaAcre: Assessment unit areas in acres if a waterbody (e.g. lake).
+#'   \item AU_Number: Unique ID formatted as numeric.
 #'   \item HUC6: Six digit basin code where the assessment unit is located.
 #'   \item HUC6_Name: Basin name where the Assessment unit is located
 #'   \item HUC8: Eight digit subbasin code where the Assessment unit is located
